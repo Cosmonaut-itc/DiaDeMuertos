46d2a1a0d82b598eb19bfa979e79a265a2fe5c5e
Felix De Haro <felixddhs@outlook.com>
2022-10-29 18:57:37 -0500

    Dia de Muertos

--- src/Pages/form.js
+++ src/Pages/form.js
@@ -4,7 +4,7 @@ function Form() {
     return (
         <div className="div-form">
             <body className="form-header">
-            <form name="registro" netlify>
+            <form action="/" method="POST" name="registro" netlify>
                 <p>
                     <label>Nombre <input type="text" name="name" /></label>
                 </p>
