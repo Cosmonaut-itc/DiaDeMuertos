5979c961a8d5e23e61819a9b8ebd953c6c5666d8
Felix De Haro <felixddhs@outlook.com>
2022-11-02 12:44:07 -0600

    fixed movement of objects

--- src/Pages/despedida.jsx
+++ src/Pages/despedida.jsx
@@ -7,7 +7,7 @@ function Despedida(){
             <body className="body-despedida">
                 <p className="titulo">¡Gracias!<br/></p>
                 <p className="parrafo"> Recuerda ir maquillado.<br/> La aportación será para apoyar <br/> con los costos de la fiesta.<br/>
-                    Envíar el comprobante a cualquier organizador </p>
+                    Envíar el comprobante a cualquier organizador <br/> Al 33 1417 5519 <br/> o <br/> Al 3315046566 </p>
             </body>
         </div>
     )
